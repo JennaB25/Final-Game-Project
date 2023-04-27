b3d0a509d4a2defe8e9dcbbed0271719223b077c
362700239 <jennarbloor@gmail.com>
2023-04-27 10:46:17 -0400

    Add project files.

---
+++ Final Game Project/Game1.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Final_Game_Project
+{
+    public class Game1 : Game
+    {      
+        enum Screen
+        {
+            Intro,
+            Options,
+            OpeningAnimation,
+            End
+        }
+        Screen screen;
+        MouseState mouseState;
+        Texture2D introScreenTexture;
+        Rectangle introScreenRect;
+        Texture2D titleTexture;
+        Rectangle titleRect;
+        SpriteFont arcadeClassicFont;
+        Texture2D paperBackgroundTexture;
+        Rectangle paperBackgroundRect;
+        Rectangle paperBackgroundRect2;
+        Texture2D optionsBackgroundTexture;
+        Rectangle optionsBackgroundRect;
+
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
+            _graphics.ApplyChanges();
+            this.Window.Title = "Kovari Mail";
+
+            screen = Screen.Intro;
+            introScreenRect = new Rectangle(0, 0, 800, 600);
+            titleRect = new Rectangle(130, 100, 550, 100);
+            paperBackgroundRect = new Rectangle(130, 250, 550, 300);
+            optionsBackgroundRect = new Rectangle(0, 0, 800, 600);
+            paperBackgroundRect2 = new Rectangle(100, 50, 600, 500);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            introScreenTexture = Content.Load<Texture2D>("introscreen");
+            titleTexture = Content.Load<Texture2D>("title");
+            arcadeClassicFont = Content.Load<SpriteFont>("pixelFont");
+            paperBackgroundTexture = Content.Load<Texture2D>("paper");
+            optionsBackgroundTexture = Content.Load<Texture2D>("optionsScreen");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (screen == Screen.Intro)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.M))
+                    screen = Screen.OpeningAnimation;
+                if (Keyboard.GetState().IsKeyDown(Keys.O))
+                    screen = Screen.Options;
+            }
+            if (screen == Screen.Options)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.B))
+                    screen = Screen.Intro;
+            }
+            if (screen == Screen.OpeningAnimation)
+            {
+                //
+            }
+
+                base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Bisque);
+
+            _spriteBatch.Begin();
+            if (screen == Screen.Intro)
+            {
+                _spriteBatch.Draw(introScreenTexture, introScreenRect, Color.White);
+                _spriteBatch.Draw(titleTexture, titleRect, Color.White);
+                _spriteBatch.Draw(paperBackgroundTexture, paperBackgroundRect, Color.White);
+                _spriteBatch.DrawString(arcadeClassicFont, "Press  M  to  Start", new Vector2(220, 330), Color.Black);
+                _spriteBatch.DrawString(arcadeClassicFont, "Press  O  for  Options", new Vector2(185, 400), Color.Black);
+            }
+            if (screen == Screen.Options)
+            {
+                _spriteBatch.Draw(optionsBackgroundTexture, optionsBackgroundRect, Color.White);
+                _spriteBatch.Draw(paperBackgroundTexture, paperBackgroundRect2, Color.White);
+                _spriteBatch.DrawString(arcadeClassicFont, "Instructions", new Vector2(245, 130), Color.Black);
+            }
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
